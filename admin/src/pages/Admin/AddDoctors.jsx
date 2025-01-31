2ac6ce4359b86e7795d9d30c8d97ca0e13d16328
Bibhash Kumar <bibhashkumar4587@gmail.com>
2025-01-31 12:08:11 +0530

    ok

--- admin/src/pages/Admin/AddDoctors.jsx
+++ admin/src/pages/Admin/AddDoctors.jsx
@@ -1,11 +1,153 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
 const AddDoctors = () => {
-  return (
-    <div>
-       <div>AddDoctors</div>
-    </div>
-  )
-}
+  const [docImg, setDocImg] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [experience, setExperience] = useState("");
+  const [fee, SteFee] = useState("");
+  const [about, setAbout] = useState("");
+  const [Speciality, setSpeciality] = useState("");
+  const [degree, setDegree] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
 
-export default AddDoctors
+  return (
+    <form className="m-5  w-full">
+      <p className="mb-3 text-lg font-medium">Add Doctor</p>
+      <div className="bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-scroll">
+        <div className="flex items-center gap-4 mb-8 text-gray-500">
+          <label htmlFor="fileInput">
+            {" "}
+            <img
+              className="w-16 bg-gray-100 rounded-full cursor-pointer"
+              src={assets.upload_area}
+              alt=""
+            />
+          </label>
+          <input type="file" id="fileInput" hidden />
+          <p>
+            Upload doctor <br /> picture
+          </p>
+        </div>
+
+        <div className="flex flex-col lg:flex-row items-start gap-10 text-gray-500">
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Doctor Name</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Name"
+                required
+              />
+            </div>
+
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Doctor Email</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Doctor Password</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Experience</p>
+              <select className="border rounded px-3 py-2" name="" id="">
+                <option value="1 year">1 year</option>
+                <option value="2 year">2 year</option>
+                <option value="3 year">3 year</option>
+                <option value="4 year">4 year</option>
+                <option value="5 year">5 year</option>
+                <option value="6 year">6 year</option>
+                <option value="7 year">7 year</option>
+                <option value="8 year">8 year</option>
+                <option value="9 year">9 year</option>
+                <option value="10 year">10 year</option>
+              </select>
+            </div>
+
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Fees</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="Number"
+                placeholder="fees"
+                required
+              />
+            </div>
+          </div>
+
+          {/* Right */}
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Speciality</p>
+              <select className="border rounded px-3 py-2" name="" id="">
+                <option value="General physician">General physician</option>
+                <option value="Gynecologist">Gynecologist</option>
+                <option value="Dermatologist">Dermatologist</option>
+                <option value="Pediatricians">Pediatricians</option>
+                <option value="Neurologist">Neurologist</option>
+                <option value="Gastroenterologist">Gastroenterologist</option>
+              </select>
+            </div>
+
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Education</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Education"
+                required
+              />
+            </div>
+
+            <div className="flex flex-1 gap-1 flex-col">
+              <p>Address</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Address 1"
+              />
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Address 2"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-1 gap-1 flex-col text-gray-500">
+          <p className="mt-4 mb-2">About Doctor</p>
+          <textarea
+            className="w-full px-4 pt-2 border rounded"
+            placeholder="write about doctor"
+            rows={3}
+            required
+          ></textarea>
+        </div>
+
+        <button className="bg-primary mt-4 px-10 py-3 text-white rounded-full">
+          Add Doctor
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddDoctors;
