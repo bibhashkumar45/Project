b662264e622d2b860f88a392e5537ff6862e6c5f
Bibhash Kumar <bibhashkumar4587@gmail.com>
2025-02-17 00:56:58 +0530

    Ok

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,79 +1,133 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { AppContext } from '../context/AppContext'
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const MyAppointments = () => {
-  const {token}=useContext(AppContext);
-  const [appointments,setAppointments]=useState([]);
-  const month=["","jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  const { token, getDoctorData } = useContext(AppContext);
+  const [appointments, setAppointments] = useState([]);
+  const month = [
+    "",
+    "jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-  const slotDataFormat=(slotDate)=>
-  {
-    const dateArray=slotDate.split('_');
-    return dateArray[0]+" "+month[Number(dateArray[1])]+" "+dateArray[2]
-  }
+  const slotDataFormat = (slotDate) => {
+    const dateArray = slotDate.split("_");
+    return (
+      dateArray[0] + " " + month[Number(dateArray[1])] + " " + dateArray[2]
+    );
+  };
 
-  const getUserAppointments=async()=>
-  {
-    try{
-      const {data}=await axios.get('http://localhost:4000/api/user/appointments',{headers:{token}});
+  const getUserAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/user/appointments",
+        { headers: { token } }
+      );
 
-      if(data.success)
-      {
+      if (data.success) {
         setAppointments(data.appointments.reverse());
-        console.log(data.appointments);
+        // console.log(data.appointments);
       }
-
-    }
-    catch(error)
-    {
+    } catch (error) {
       console.log(error);
       toast.error(error.message);
-
     }
-  }
-  
-  useEffect(()=>
-  {
-    if(token)
-    {
+  };
+
+  const cancleApppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/api/user/cancle-appointment",
+        { appointmentId },
+        { headers: { token } }
+      );
+      console.log("not working");
+      if (data.success) {
+        toast.success(data.message);
+        getUserAppointments();
+        getDoctorData();
+      } else {
+        toast.error(data.message);
+      }
+    } catch {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
       getUserAppointments();
     }
-
-  },[token]);
+  }, [token]);
 
   return (
     <div>
-    <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
-    <div >
-      {
-       appointments.map((item,index)=>
-        (
-          <div 
-          className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b'
-           key={index}>
+      <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">
+        My appointments
+      </p>
+      <div>
+        {appointments.map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b"
+            key={index}
+          >
             <div>
-              <img className='w-32 bg-indigo-200' src={item.docData.image} alt="" />
+              <img
+                className="w-32 bg-indigo-200"
+                src={item.docData.image}
+                alt=""
+              />
             </div>
-            <div className='flex-1 text-sm text-zinc-600'>
-              <p className='text-neutral-800 font-semibold'>{item.docData.name}</p>
+            <div className="flex-1 text-sm text-zinc-600">
+              <p className="text-neutral-800 font-semibold">
+                {item.docData.name}
+              </p>
               <p>{item.docData.speciality}</p>
-              <p className='text-zinc-700 font-medium mt-1'>Address:</p>
-              <p className='text-xs'>{item.docData.address.line1}</p>
+              <p className="text-zinc-700 font-medium mt-1">Address:</p>
+              <p className="text-xs">{item.docData.address.line1}</p>
               <p>{item.docData.address.line2}</p>
-              <p className='text-xm mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time: </span> {slotDataFormat(item.slotData)}|  {item.slotTime}</p>
+              <p className="text-xm mt-1">
+                <span className="text-sm text-neutral-700 font-medium">
+                  Date & Time:{" "}
+                </span>{" "}
+                {slotDataFormat(item.slotData)}| {item.slotTime}
+              </p>
             </div>
             <div></div>
-            <div className='flex flex-col justify-end gap-2'>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-500'>Pay Online</button>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded  hover:bg-red-600 hover:text-white transition-all duration-500'>Cancel appointment</button>
+            <div className="flex flex-col justify-end gap-2">
+              {!item.cancelled && (
+                <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-500">
+                  Pay Online
+                </button>
+              )}
+              {!item.cancelled && (
+                <button
+                  onClick={() => cancleApppointment(item._id)}
+                  className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded  hover:bg-red-600 hover:text-white transition-all duration-500"
+                >
+                  Cancel appointment
+                </button>
+              )}
+              {item.cancelled && <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">Appointment cancelled</button>}
             </div>
           </div>
-        ))
-      }
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default MyAppointments
+export default MyAppointments;
